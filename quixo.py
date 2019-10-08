e4d2cfc7cf21ab0466d1c5fc89645f0e68a0e645
Mauro Bailon <reekremag@gmail.com>
2019-10-08 16:12:14 -0300

    add few changes

--- quixo.py
+++ quixo.py
@@ -2,27 +2,11 @@ class Quixo:
 
     def __init__(self):
         self.board = [[0 for x in range(5)] for y in range(5)]
-        self.dict = self.create_dict()
-
-    def create_dict(self):
-        temp_dict = []
-        temp_dict[1] = (0,0)
-        temp_dict[2] = (0, 1)
-        temp_dict[3] = (0, 2)
-        temp_dict[4] = (0, 3)
-        temp_dict[5] = (0, 4)
-        temp_dict[6] = (1, 4)
-        temp_dict[7] = (2, 4)
-        temp_dict[8] = (3, 4)
-        temp_dict[9] = (4, 4)
-        temp_dict[10] = (4, 3)
-        temp_dict[11] = (4, 2)
-        temp_dict[12] = (4, 1)
-        temp_dict[13] = (4, 0)
-        temp_dict[14] = (3, 0)
-        temp_dict[15] = (2, 0)
-        temp_dict[16] = (1, 0)
-        return temp_dict
+        self.edges = [(100, 100),
+                      (0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
+                      (1, 4), (2, 4), (3, 4), (4, 4),
+                      (4, 3), (4, 2), (4, 1), (4, 0),
+                      (3, 0), (2, 0), (1, 0)]
 
     def modify_board(self, origin, destiny):
         if self.board[origin[0]][origin[1]] == 0:
@@ -37,17 +21,28 @@ class Quixo:
             self.push_column(0) #abajo
 
     def push_column(self, direction):
-
+        pass
 
     def push_line(self, direction):
-
+        pass
 
     def player_play(self):
         pass
 
-    def oponent_play(self, move):
-        origin = self.dict[move[0]]
-        destiny = self.dict[move[1]]
-        self.modify_board(origin, destiny)
+    def check_movement(self, move):
         pass
+
+    @staticmethod
+    def valid_movement(self, origin, destiny):
+        return origin[0] == destiny[0] or origin[1] == destiny[1]
+
+    def opponent_play(self, move):
+        origin = self.edges[move[0]]
+        destiny = self.edges[move[1]]
+
+        if not self.valid_movement(origin, destiny):
+            raise Exception("Invalid movement of the opponent")
+
+        self.modify_board(origin, destiny)
+
 
